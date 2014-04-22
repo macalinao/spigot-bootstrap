e2c69731ca02145b3cf6b6cded704b0b91bdd833
Ian Macalinao <ian@giza.us>
2014-04-21 20:58:54 -0500

    Don't rm but use -N instead
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- update.sh
+++ update.sh
@@ -1,3 +1,1 @@
-rm spigot.jar
-
-wget http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar
+wget -N http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar
