76eb5216b1bbb6eb9b22cf8fc7709ad0e5989f4e
Ian Macalinao <ian@giza.us>
2014-05-12 21:02:39 -0500

    Add plugins to the update script
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
 wget -N http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar
+
+# Plugins
+wget -P plugins/ -N http://dev.bukkit.org/media/files/785/673/ProtocolLib-3.3.1.jar
