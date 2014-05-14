5fb5993e89101b57bd1f3d2db9d4bb632ff68f34
Ian Macalinao <ian@giza.us>
2014-05-14 00:15:02 -0500

    Add essentials to update script
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- update.sh
+++ update.sh
@@ -2,3 +2,8 @@ wget -N http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server
 
 # Plugins
 wget -P plugins/ -N http://dev.bukkit.org/media/files/785/673/ProtocolLib-3.3.1.jar
+
+# Essentials
+wget -P plugins/ -N http://dev.bukkit.org/media/files/780/922/Essentials.zip
+unzip -d plugins/ plugins/Essentials.zip
+rm plugins/Essentials.zip
