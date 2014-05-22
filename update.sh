b5ddf9e44efc9936a702e456642bbd1c93ce17de
Ian Macalinao <ian@giza.us>
2014-05-21 21:33:19 -0500

    Add more plugins
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ wget -N http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server
 wget -P plugins/ -N http://dev.bukkit.org/media/files/785/673/ProtocolLib-3.3.1.jar
 wget -P plugins/ -N http://dev.bukkit.org/media/files/786/103/worldedit-5.6.2.jar
 wget -P plugins/ -N http://dev.bukkit.org/media/files/776/555/worldguard-5.9.jar
+wget -P plugins/ -N http://dev.bukkit.org/media/files/696/572/Factions.jar
 
 # Essentials
 wget -P plugins/ -N http://dev.bukkit.org/media/files/780/922/Essentials.zip
